0a8ee1363f10e3f3a0f458ae8c3a9607386ae8db
Kris Pinespear <pinespear@outlook.com>
2020-11-16 02:16:08 -0600

    Initial implementation of ord_eq_by_key_selector

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # rust_ord_by_key
-Rust macro for implementing Ord trait with logic specified in an inline expression
+Provides a convenient macro for implementing Ord trait with logic
+specified in an inline expression
+
+```rust
+use core::cmp::Reverse;
+// `Person` will be ordered by `last_name`, then by `first_name`, then by `age` in reverse
+#[ord_eq_by_key_selector(|p|
+    &p.last_name,
+    &p.first_name,
+    Reverse(p.age),)]
+pub struct Person {
+    pub first_name: String,
+    pub last_name: String,
+    pub age: usize,
+}
+```
+
+```rust
+// Container for `&str` which will be ordered by underlying string length
+#[ord_eq_by_key_selector(|s| s.0.len())]
+pub struct StrByLen<'a> (&'a str);
+
+assert!(StrByLen("Alex") > StrByLen("Bob"))
+```
 
 # License
 Distributed under the terms of both the MIT license and the Apache License (Version 2.0)
---
+++ src/lib.rs
@@ -0,0 +1,333 @@
+//! Provides a convenient macro for implementing Ord trait with logic
+//! specified in an inline expression
+//!
+//! ```
+//! # #[macro_use] extern crate ord_by_key;
+//! use core::cmp::Reverse;
+//! // `Person` will be ordered by `last_name`, then by `first_name`, then by `age` in reverse
+//! #[ord_eq_by_key_selector(|p|
+//!     &p.last_name,
+//!     &p.first_name,
+//!     Reverse(p.age),)]
+//! pub struct Person {
+//!     pub first_name: String,
+//!     pub last_name: String,
+//!     pub age: usize,
+//! }
+//! ```
+//!
+//! ```
+//! # #[macro_use] extern crate ord_by_key;
+//! // Container for [`&str`] which will be ordered by underlying string length
+//! #[ord_eq_by_key_selector(|s| s.0.len())]
+//! pub struct StrByLen<'a> (&'a str);
+//!
+//! assert!(StrByLen("Alex") > StrByLen("Bob"))
+//! ```
+
+#![deny(missing_docs)]
+#![deny(warnings)]
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::parse::Parse;
+use syn::parse::ParseStream;
+use syn::Expr;
+use syn::Ident;
+use syn::ItemStruct;
+use syn::Token;
+
+#[doc(inline)]
+/// Implements [`Ord`], [`PartialOrd`], [`PartialEq`] and [`Eq`] for a struct.
+///
+/// Implemented comparison logic is based on a custom key extraction expression provided.
+/// During comparison, key extraction expression will be evaluated for both values and then
+/// resulted keys will be compared with each other. `ord_eq_by_key_selector` supports
+/// multiple key expressions, which allows implement logic "sort by ..., then by ..."
+///
+/// # Syntax
+/// ```compile_fail
+/// #[ord_eq_by_key_selector(|parameter_name| key_expressoin, key_expressoin, ...)]
+/// pub struct MyStruct {...}
+/// ```
+/// * `parameter_name` - name of the parameter which will contain reference to the struct
+/// value from which key is being extracted (type `&MyStruct`). It can be any identifier
+/// (similar to how parameters are defined in a closure).
+/// * `key_expression` - expression which produces a key for comparison. Expression can
+/// access `parameter_name` input and must return `impl Ord`. Multiple expressions can be
+/// provided, comma-separated (last comma is optional). Expression can be single-line, or
+/// multi-line enclosed in `{}`.
+/// * `pub struct MyStruct {...}` - definition of struct for which [`Ord`], [`PartialOrd`],
+/// [`PartialEq`] and [`Eq`] will be implemented
+///
+/// # Comparison logic
+/// Let's look at [`Ord::cmp`] implementation (rest of traits have similar implementation logic)
+///
+/// When 2 values `a` and `b` are compared using auto-implemented comparison method, following
+/// logic will be executed:
+///
+/// 1) use specified `key_expression`, and evaluate comparison keys for `a` and `b` (`key_a` and `key_b`)
+/// 2) compare `key_a` and `key_b` using their own [`Ord`] implementaiton
+/// 3) If comparisong result is not [`::core::cmp::Ordering::Equal`], return comparison reslut
+/// 4) Otherwise switch to next provided key expression and redo steps 1..4 using this expresison
+/// 5) If no key expressions left, return [`::core::cmp::Ordering::Equal`]
+///
+/// Note, all evaluations are lazy and happen only when they are necessary. For example, if first
+/// provided key expression gave us non-ambigious result, then rest of key expressions will not be
+/// evaluated.
+///
+/// # Key Expression implementation considerations and examples
+///
+/// In simple case, key expression returns one of fields from the underlying struct.
+/// ```
+/// # #[macro_use] extern crate ord_by_key;
+/// // `Person` will be ordered by it's field `age`
+/// #[ord_eq_by_key_selector(|p| p.age)]
+/// pub struct Person {
+///     pub first_name: String,
+///     pub last_name: String,
+///     pub age: usize,
+/// }
+/// ```
+///
+/// If return type does not implement [`Copy`], expression should return borrowed value
+/// ```
+/// # #[macro_use] extern crate ord_by_key;
+/// // `Person` will be ordered by it's field `last_name`
+/// #[ord_eq_by_key_selector(|p| &p.last_name)]
+/// pub struct Person {
+///     pub first_name: String,
+///     pub last_name: String,
+///     pub age: usize,
+/// }
+/// ```
+///
+/// If struct should be sorted by multiple fields, multiple expressions can be provided.
+/// Note, that parameter name should be specified only once, and each of expressions can
+/// use it
+/// ```
+/// # #[macro_use] extern crate ord_by_key;
+/// // `Person` will be ordered by `last_name`, then by `first_name`
+/// #[ord_eq_by_key_selector(|p|
+///     &p.last_name,
+///     &p.first_name, )]
+/// pub struct Person {
+///     pub first_name: String,
+///     pub last_name: String,
+///     pub age: usize,
+/// }
+/// ```
+///
+/// If struct should be sorted by some keys in reverse order, you can use [`::core::cmp::Reverse`]
+/// container for values which should be sorted in reverse:
+/// ```
+/// # #[macro_use] extern crate ord_by_key;
+/// use core::cmp::Reverse;
+/// // `Person` will be ordered by `last_name`, then by `first_name`, then by `age` in reverse
+/// #[ord_eq_by_key_selector(|p|
+///     &p.last_name,
+///     &p.first_name,
+///     Reverse(p.age),)]
+/// pub struct Person {
+///     pub first_name: String,
+///     pub last_name: String,
+///     pub age: usize,
+/// }
+/// ```
+///
+/// You can use multi-line block expression and access multiple fields. You can use explicit
+/// `return`
+/// ```
+/// # #[macro_use] extern crate ord_by_key;
+/// // Sort Point by distance from the (0,0)
+/// #[ord_eq_by_key_selector(|p|
+///     {
+///         let x = p.x as i64;
+///         let y = p.y as i64;
+///
+///         // For sorting purposes, it's not necessary to take square root
+///         let distance = x * x + y * y;
+///         
+///         return distance;
+///     })]
+/// pub struct Point {
+///     pub x: i32,
+///     pub y: i32,
+/// }
+/// ```
+///
+/// Note that all expressions are lazy evaluated every time comparison is triggered. In
+/// some applications that can lead to low performance if key expressions are computationally
+/// expensive and comparisons happen repeatedly.
+///
+/// # Custom sorting logic for existing structs
+/// One of use case is introduction of custom sorting logic to existing structs or different
+/// sorting logic for different cases. Example how custom logic is introduces in core library
+/// is [`::core::cmp::Reverse`] container (which reverses existing comparison logic), but we can
+/// implement more complicated containers.
+///
+/// Let's say, we want to sort integers by their absolute value, and then by the actual value.
+/// We cannot introduce new sort logic to i32 because it already implements [`Ord`], but we can
+/// introduce container for i32 which has custom sorting logic. Note that we are using struct
+/// with anonymous field and have to access field using `.0`
+/// ```
+/// # #[macro_use] extern crate ord_by_key;
+/// // Container for [`i32`] will be ordered by absolute value, then by actual values
+/// #[ord_eq_by_key_selector(|i| i.0.abs(), i.0)]
+/// pub struct I32ByAbs(i32);
+///
+/// let a = I32ByAbs(10);
+/// let b = I32ByAbs(-11);
+///
+/// assert!(a < b);
+/// ```
+///
+/// You can use more complicated containers with generic and constraints
+/// ```
+/// # #[macro_use] extern crate ord_by_key;
+/// use core::cmp::Reverse;
+/// use std::collections::BinaryHeap;
+/// use std::fmt::Debug;
+///
+/// // Merges sorted iterator and prints final sorted sequence to the console
+/// fn merge_sorted_iterators<T: Ord + Debug, I: Iterator<Item = T>>(iterators: Vec<I>) {
+///     let mut heap = BinaryHeap::new();
+///
+///     // Container for iterator and it's last value, ordered by value
+///     #[ord_eq_by_key_selector(|i| &i.0)]
+///     // Note that inner struct cannot use generic parameters from the outer function
+///     // (error[E0401]) so they have to be re-defined with constraints.
+///     struct ItemIter<T: Ord + Debug, I: Iterator<Item = T>>(T, I);
+///
+///     for mut iterator in iterators {
+///         if let Some(item) = iterator.next() {
+///             // Need to Reverse here because BinaryHeap by default works as max heap
+///             // and we need min heap
+///             heap.push(Reverse(ItemIter(item, iterator)));
+///         }
+///     }
+///
+///     while let Some(Reverse(ItemIter(item, mut iterator))) = heap.pop() {
+///         println!("{:?}", item);
+///
+///         if let Some(item) = iterator.next() {
+///             heap.push(Reverse(ItemIter(item, iterator)));
+///         }
+///     }
+/// }
+/// ```
+///
+#[proc_macro_attribute]
+pub fn ord_eq_by_key_selector(attr: TokenStream, item: TokenStream) -> TokenStream {
+    let attr = syn::parse_macro_input!(attr as MacroAttribute);
+
+    let key_selector_param = &attr.param;
+    let key_selectors = &attr.key_selectors;
+    let key_selector_func_names: Vec<_> = (0..key_selectors.len())
+        .map(|i| format!("_ord_eq_by_key_selector_{}", i))
+        .map(|n| Ident::new(&n, proc_macro2::Span::mixed_site()))
+        .collect();
+
+    let structure = syn::parse_macro_input!(item as ItemStruct);
+    let structure_name = &structure.ident;
+    let generics = &structure.generics;
+
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let result = quote! {
+        #structure
+
+        impl #impl_generics #structure_name #ty_generics #where_clause {
+            #(
+                fn #key_selector_func_names  (#key_selector_param: &Self) -> impl ::core::cmp::Ord + '_ {
+                    #key_selectors
+                }
+            )*
+        }
+
+        impl #impl_generics ::core::cmp::PartialEq for #structure_name #ty_generics #where_clause {
+            fn eq(&self, other: &Self) -> bool {
+                #(
+                    let key_self = #structure_name::#key_selector_func_names(self);
+                    let key_other = #structure_name::#key_selector_func_names(other);
+
+                    let result = key_self.eq(&key_other);
+
+                    if result != false {
+                        return result;
+                    }
+                )*
+
+                return true;
+            }
+        }
+
+        impl #impl_generics ::core::cmp::Eq for #structure_name #ty_generics #where_clause { }
+
+        impl #impl_generics ::core::cmp::Ord for #structure_name #ty_generics #where_clause {
+            fn cmp(&self, other: &Self) -> ::core::cmp::Ordering {
+                #(
+                    let key_self = #structure_name::#key_selector_func_names(self);
+                    let key_other = #structure_name::#key_selector_func_names(other);
+
+                    let result = key_self.cmp(&key_other);
+
+                    if result != ::core::cmp::Ordering::Equal {
+                        return result;
+                    }
+                )*
+
+                return ::core::cmp::Ordering::Equal;
+            }
+        }
+
+        impl #impl_generics ::core::cmp::PartialOrd for #structure_name #ty_generics #where_clause {
+            fn partial_cmp(&self, other: &Self) -> ::core::option::Option<::core::cmp::Ordering> {
+                ::core::option::Option::Some(self.cmp(other))
+            }
+        }
+    };
+
+    result.into()
+}
+
+/// container for syntax of attribute
+/// | ident | expression, expression, ....
+/// There has to be at least one expression, comma-delimited
+/// Last comma is optional
+struct MacroAttribute {
+    _bar1: Token![|],
+    param: Ident,
+    _bar2: Token![|],
+    key_selectors: Vec<Expr>,
+}
+
+impl Parse for MacroAttribute {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        Ok(MacroAttribute {
+            _bar1: input.parse()?,
+            param: input.parse()?,
+            _bar2: input.parse()?,
+            key_selectors: {
+                let mut exprs = vec![];
+
+                loop {
+                    let expr: Expr = input.parse()?;
+                    exprs.push(expr);
+
+                    if input.is_empty() {
+                        break;
+                    }
+
+                    let _: Token!(,) = input.parse()?;
+
+                    if input.is_empty() {
+                        break;
+                    }
+                }
+
+                exprs
+            },
+        })
+    }
+}
